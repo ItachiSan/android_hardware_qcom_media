a6c016789a50102448c7c59a6a06d14ca36e81c1
Giovanni Santini <giovannisantini93@yahoo.it>
2014-03-04 09:33:16 +0100

    Added missing libI420colorconvert Took from CAF ics_chocolate qcom_media
    Change-Id: I66c71f7bfb2c822084dc19d62194f2e8515a20db

--- Android.mk
+++ Android.mk
@@ -1,7 +1,8 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
-	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
-endif
+    ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
+	    include $(QCOM_MEDIA_ROOT)/audio/Android.mk
+    endif
+    include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
 endif
