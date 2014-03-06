31764b7bcd516db6b81e782d2a6da11fdd82e4ea
Giovanni Santini <giovannisantini93@yahoo.it>
2014-03-06 15:16:20 +0100

    Legacify audio In this way we can use CAF one
    Change-Id: I51ef9101a74e1b62da688c38988f67f444cbcdf2

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ QCOM_MEDIA_ROOT := $(call my-dir)
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
-	    include $(QCOM_MEDIA_ROOT)/audio/Android.mk
+	    include $(QCOM_MEDIA_ROOT)/audio-legacy/Android.mk
     endif
     include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
 endif
