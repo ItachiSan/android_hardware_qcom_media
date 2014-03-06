454fb75b160214bb2a4a36a83b50096946dd199f
Giovanni Santini <giovannisantini93@yahoo.it>
2014-03-06 17:31:07 +0100

    Actually legacify Derps fixed
    Change-Id: Ifcecb15f5352a2e2680e087b33e1a54217119d1e

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-    ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
+    ifeq ($(BOARD_USES_AUDIO_LEGACY),true)
 	    include $(QCOM_MEDIA_ROOT)/audio-legacy/Android.mk
     endif
     include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
